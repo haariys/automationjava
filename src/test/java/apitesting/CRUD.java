ae36b53ac49072fa7e0d11afc977f0faf032176e
haris <haariys@gmail.com>
2023-06-01 14:27:55 +0500

    JSON schema validation in restassured

--- src/test/java/apitesting/CRUD.java
+++ src/test/java/apitesting/CRUD.java
@@ -1,21 +1,33 @@
 package apitesting;
+
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+
 import static io.restassured.RestAssured.*;
+import static io.restassured.response.Response.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertTrue;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class CRUD {
 	@Test
-	public void getResponseBody() { 
-		   
-		given()
-		  .when()
-		  .get("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1")
-		  .then()
-		  .assertThat().statusCode(200)
-		  .log()
-		  .all();
-	    	
+	public void getResponseBody() {
+
+		baseURI = "https://reqres.in/";
+
+//		ValidatableResponse response = given().when().get("/api/users/2").then().assertThat().statusCode(200)
+//				.assertThat().body(containsString("janet.weaver@reqres.in")).body("data.first_name", equalTo("Janet"))
+//				.log().all();
+		
+		
+				
+		get("/api/users?page=2").then().assertThat().body(matchesJsonSchemaInClasspath("schema.json"));
+
 	}
 }
